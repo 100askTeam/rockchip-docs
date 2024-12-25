da12383176dd63cc4d89dd0c27814cacb207cd61
meihao@ccy <meihao@ccy.com>
2024-12-25 17:16:46 +0800

    add 100ask rk3568 manual files

---
+++ docs/Rockchip-SBC/intro.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 1
+---
+
+# DshanPI-RockChip
+
+ **从DshanPI-RockChip开启您的嵌入式之旅**.
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,11 +9,11 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: '东山Π',
-  tagline: 'DshanPI Linux Boards Docs.',
+  tagline: 'DshanPI Rockchip Boards Docs.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://dshanpi.100ask.net',
+  url: 'https://rockchip.100ask.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: '100askTeam', // Usually your GitHub org/user name.
-  projectName: 'linuxboard-docs', // Usually your repo name.
+  projectName: 'rockchip-docs', // Usually your repo name.
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
@@ -66,72 +66,30 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: '东山Π',
+        title: 'rockchip >',
         logo: {
           alt: '东山PI',
           src: 'img/logo.svg',
         },
-        items: [
+        items: [         
           {
             type: 'docSidebar',
-            sidebarId: 't113s3Sidebar',
+            sidebarId: 'rockchipsbcSidebar',
             position: 'left',
-            label: 'T113s3-Industrial',
-          },          
-          {
-            type: 'docSidebar',
-            sidebarId: 'v851seSidebar',
-            position: 'left',
-            label: 'TinyVision',
+            label: 'Rockchip SBC 介绍',
           },
           {
             type: 'docSidebar',
-            sidebarId: 'd1hdSidebar',
+            sidebarId: 'rk3568Sidebar',
             position: 'left',
-            label: 'D1h-DualDisplay',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 'mp135dkSidebar',
-            position: 'left',
-            label: 'MP135-DevKit',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 'd1scvbsSidebar',
-            position: 'left',
-            label: 'D1s-CVBS',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 't113iSidebar',
-            position: 'left',
-            label: 'T113i-Industrial',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 't113s3proSidebar',
-            position: 'left',
-            label: 'T113s3-Pro',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 'r818dshanpirosxSidebar',
-            position: 'left',
-            label: 'R818-DshanPI-ROSx',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 'h616Sidebar',
-            position: 'left',
-            label: 'H616-YuzukiHD-Chameleon',
-          },         
+            label: '100ASK-RK3568 开发文档',
+          },        
           {
             type: 'localeDropdown',
             position: 'right',
           },
           {
-            href: 'https://github.com/100askTeam/linuxboard-docs',
+            href: 'https://github.com/100askTeam/rockchip-docs',
             label: 'GitHub',
             position: 'right',
           },
@@ -140,77 +98,6 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'DongshanPI',
-                href: 'https://dongshanpi.com',
-              },
-              {
-                label: 'Canaan-Docs',
-                href: 'https://canaan-docs.100ask.net/',
-              },
-              {
-                label: 'Renesas-Docs',
-                href: 'https://renesas-docs.100ask.net/',
-              },
-              {
-                label: 'RTOS',
-                href: 'https://rtos.100ask.net/',
-              },
-              {
-                label: 'TinaSDK-Docs',
-                href: 'https://tina.100ask.net/',
-              },
-              {
-                label: 'Allwinner-Docs',
-                href: 'https://allwinner-docs.100ask.net/',
-              },
-              {
-                label: 'R128-Docs',
-                href: 'https://aw-r128.100ask.net/',
-              },                                                                                    
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'BiliBili',
-                href: 'https://space.bilibili.com/275908810',
-              },              
-              {
-                label: 'Stack Overflow',
-                href: 'https://forums.100ask.net',
-              },
-              {
-                label: 'VideoCenter',
-                href: 'https://video.100ask.net/',
-              },              
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/dongshanpi',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Coding',
-                href: 'https://weidongshan.coding.net/public/',
-              },              
-              {
-                label: 'GitHub',
-                href: 'https://github.com/100askTeam',
-              },
-              {
-                label: 'Gitee',
-                href: 'https://gitee.com/weidongshan',
-              },              
-            ],
-          },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} 100askTeam, Inc. Built with Docusaurus.`,
       },
